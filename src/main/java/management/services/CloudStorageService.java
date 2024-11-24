bced58ad171d6133078bf1749a87d3f6366a8f01
bensagi <bensagi@cisco.com>
2024-11-24 13:04:49 +0200

    impl

--- src/main/java/management/services/CloudStorageService.java
+++ src/main/java/management/services/CloudStorageService.java
@@ -1,21 +1,27 @@
 package management.services;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.CopyWriter;
 import com.google.cloud.storage.StorageOptions;
+import io.micrometer.common.util.StringUtils;
 import java.io.IOException;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import com.google.cloud.storage.Storage;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class CloudStorageService {
 
+  public static final String GS = "gs://";
   private final Storage storageClient;
+
+  private final Logger logger = LoggerFactory.getLogger(CloudStorageService.class);
+
 
   public CloudStorageService() throws IOException {
     GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
@@ -31,4 +37,54 @@ public class CloudStorageService {
         .forEach(blob -> files.add("gs://" + blob.getBucket() + "/" + blob.getName()));
     return files;
   }
+
+  public String uploadFileToStorage(String bucketName, String folder, String fileName, byte[] bytes) {
+    String filePath = StringUtils.isEmpty(folder) ? fileName : folder + "/" + fileName;
+
+    BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, filePath).build();
+
+    storageClient.create(blobInfo, bytes);
+
+    logger.info("uploaded file to GCS. bucket = {}, file = {}", bucketName, filePath);
+
+    return filePath;
+  }
+
+  public String copyObject(String sourceGcsUri, String destinationBucketName, String destinationObjectName) {
+
+    // Create BlobId for source and destination
+    BlobId sourceBlobId = getGetBlobId(sourceGcsUri);
+    BlobId destinationBlobId = BlobId.of(destinationBucketName, destinationObjectName);
+    BlobInfo destinationBlobInfo = BlobInfo.newBuilder(destinationBlobId).build();
+    // Perform the copy
+    CopyWriter copyWriter = storageClient.copy(Storage.CopyRequest.newBuilder()
+        .setSource(sourceBlobId)
+        .setTarget(destinationBlobInfo)
+        .build());
+    // Get the result and print the new object's information
+    copyWriter.getResult();
+    String destinationUri = GS + destinationBucketName + "/" + destinationObjectName;
+    logger.debug("Copied object from {} to {}", sourceGcsUri, destinationUri);
+    return destinationUri;
+  }
+
+  private static BlobId getGetBlobId(String uri) {
+    String[] sourceUriParts = uri.replace("gs://", "").split("/", 2);
+    return BlobId.of(sourceUriParts[0], sourceUriParts[1]);
+  }
+
+  public boolean deleteObject(String objectUri) {
+    BlobId blobId = getGetBlobId(objectUri);
+
+    boolean deleted = storageClient.delete(blobId);
+    if (deleted) {
+      logger.debug("deleted: {}", objectUri);
+      return true;
+    } else {
+      logger.error("Failed to delete the source object: {}", objectUri, new Exception());
+      return false;
+    }
+  }
+
+
 }
--- src/main/java/management/services/ImageService.java
+++ src/main/java/management/services/ImageService.java
@@ -6,7 +6,14 @@ import com.wild_tag.model.CoordinatesApi;
 import com.wild_tag.model.ImageApi;
 import com.wild_tag.model.ImageStatusApi;
 import com.wild_tag.model.ImagesBucketApi;
+import jakarta.transaction.Transactional;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import management.entities.images.CoordinateDB;
 import management.entities.images.ImageDB;
@@ -14,14 +21,34 @@ import management.entities.images.ImageStatus;
 import management.entities.users.UserDB;
 import management.repositories.ImagesRepository;
 import management.repositories.UserRepository;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImageService {
 
+  public static final String DATASET = "dataset";
+  public static final String LABELS = "labels";
+  public static final String VAL = "val";
+  public static final String TRAIN = "train";
+  public static final String IMAGES = "images";
   private Logger logger = LoggerFactory.getLogger(ImageService.class);
+
+
+  @Value("${data_set_bucket:wild-tag-data-set}")
+  private String dataSetBucket;
+
+  @Value("${storage_root_dir:dev}")
+  private String storageRootDir;
+
+  @Value("${validate_rate:15}")
+  private Integer validateRate;
+
+  private Map<String, Integer> categoriesHistogram;
 
   private CloudStorageService cloudStorageService;
 
@@ -96,4 +123,99 @@ public class ImageService {
         setWidth(coordinatesApi.getWidth()).
         setHeight(coordinatesApi.getHeight());
   }
+
+  public List<ImageDB> getValidatedImages(int limit) {
+    return imagesRepository.getByStatus(ImageStatus.VALIDATED, PageRequest.of(0, limit));
+  }
+
+  @Transactional
+  public void buildImageTag(ImageDB imageDB) throws IOException {
+    String yoloImagePath = createYoloFiles(imageDB);
+    imageDB.setGcsTaggedPath(yoloImagePath);
+    imageDB.setStatus(ImageStatus.TRAINABLE);
+    imagesRepository.save(imageDB);
+  }
+
+  public String createYoloFiles(ImageDB imageDB) throws IOException {
+    List<CoordinateDB> coordinates = imageDB.getCoordinates(); // Assuming a getter for coordinates exists
+
+    // Create a byte array output stream to temporarily hold the file content
+    try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Writer writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
+
+      byte[] yoloTextByteArray = getYoloText(coordinates, writer, outputStream);
+
+      boolean isValidate = isValidate(imageDB);
+
+      String[] parts = imageDB.getGcsFullPath().split("/");
+      String objectName = parts[parts.length - 1];
+
+      uploadTextFile(isValidate, convertToTxtFileName(objectName), yoloTextByteArray);
+      return moveImageToDataSetPath(isValidate, imageDB.getGcsFullPath(), objectName);
+    } catch (IOException e) {
+      logger.error("failed to create yolo files for image {}", imageDB.getId(), e);
+      throw e;
+    }
+  }
+
+  public static String convertToTxtFileName(String imageFileName) {
+
+    int dotIndex = imageFileName.lastIndexOf('.');
+    if (dotIndex == -1) {
+      return imageFileName + ".txt"; // No extension found, append .txt
+    } else {
+      return imageFileName.substring(0, dotIndex) + ".txt"; // Replace existing extension
+    }
+  }
+
+
+  private String uploadTextFile(boolean isValidate, String objectName, byte[] yoloTextByteArray) {
+    String folder = String.format("%s/%s/%s/%s", storageRootDir, DATASET, LABELS, isValidate ? VAL : TRAIN);
+    return cloudStorageService.uploadFileToStorage(dataSetBucket, folder, objectName, yoloTextByteArray);
+  }
+
+  private String moveImageToDataSetPath(boolean isValidate, String gcsFullPath, String objectName) {
+
+    String destinationObject = String.format("%s/%s/%s/%s/%s", storageRootDir, DATASET, IMAGES, isValidate ? VAL : TRAIN, objectName);
+    cloudStorageService.copyObject(gcsFullPath, dataSetBucket, destinationObject);
+    String copiedImageUri = String.format("gs://%s/%s", dataSetBucket, destinationObject);
+    cloudStorageService.deleteObject(gcsFullPath);
+    return copiedImageUri;
+  }
+
+  private boolean isValidate(ImageDB imageDB) {
+    boolean isValidate = false;
+
+    for (CoordinateDB coordinateDB : imageDB.getCoordinates()) {
+      String category = coordinateDB.getAnimalId();
+      int categoryObjects = categoriesHistogram.get(category) != null ? categoriesHistogram.get(category) : 0;
+      categoriesHistogram.put(category, ++categoryObjects);
+      if (categoryObjects % validateRate == 0) {
+        isValidate = true;
+      }
+    }
+
+    return isValidate;
+  }
+
+  @NotNull
+  private static byte[] getYoloText(List<CoordinateDB> coordinates, Writer writer, ByteArrayOutputStream outputStream)
+      throws IOException {
+    for (CoordinateDB coord : coordinates) {
+      int classId = Integer.parseInt(coord.getAnimalId());
+      double xCenter = coord.getX_center();
+      double yCenter = coord.getY_center();
+      double width = coord.getWidth();
+      double height = coord.getHeight();
+
+      // Format the line in YOLO format: class x_center y_center width height
+      String line = String.format("%d %.2f %.2f %.2f %.2f", classId, xCenter, yCenter, width, height);
+      writer.write(line);
+      writer.write("\n");
+    }
+    writer.flush();
+
+    return outputStream.toByteArray();
+  }
+
 }
